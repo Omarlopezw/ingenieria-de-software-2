5680afb17749a017520e83454e93b4e5e94139a9
Omarlopezw <omarlopezxs@gmail.com>
2023-05-28 20:11:36 -0300

    adding date

--- attendanceHandler/web/web.py
+++ attendanceHandler/web/web.py
@@ -1,20 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+from datetime import datetime
 
 class WebHandler(Flask):
 
     def __init__(self,moduleName):
         super().__init__(moduleName)
-        
-    # def setRoutWebPage(self):
-        @self.route('/')
-        def getMainWebPage():
+        self.fecha_hora = ''
+        @self.route('/', methods=['GET', 'POST'])
+        def getMainWebPage():   
+            if request.method == 'POST' or request.method == 'GET':
+                self.fecha_hora = datetime.now()
             return render_template("index.html")
         
-        @self.route('/terceranio/seminario')
-        def getSeminarioWebPage():
-            return render_template("index.html")
-            # return "¡Esta es otra ruta!"
+        @self.route('/form-example',methods=['GET', 'POST'])
+        def getData():
+            field1 = request.form.get('nombre')
+            field2 = request.form.get('apellido')
+            field3 = request.form.get('cuilt')
 
+            return '''
+                <h1>Con sus límites:</h1>
+                <ul>
+                    <li><h1>Name: {}</h1></li>
+                    <li><h1>Apellido:= {}</h1></li>
+                    <li><h1>CUILT: {}</h1></li>
+                    <li><h1>Date: {}</h1></li>
+                </ul>
+                '''.format(field1,field2,field3,self.fecha_hora)
+    def getDate(date):
+        return date
+
+    
+app = WebHandler(__name__)
+
+print()
+    
 if __name__ == '__main__':
-    app = WebHandler(__name__)
     app.run()
